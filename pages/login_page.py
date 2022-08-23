01ea56750e945132716a40cb61cfdc9199642af5
siuplp <syuzanna.pylypiuk@gmail.com>
2022-08-24 00:51:03 +0300

    third.task

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,10 +4,18 @@ from pages.base_page import BasePage
 class LoginPage(BasePage):
     scout_panel_xpath = "//h5[contains(text(),'Scouts Panel')]"
     password_xpath ="input[@id='password']"
-    remind_password _xpath ="a[contains(text(),'Remind password')]"
+    remind_password_xpath ="a[contains(text(),'Remind password')]"
     change_language_xpath ="//body[1]/div[1]/form[1]/div[1]/div[2]/div[1]/div[1]"
     login_field_xpath = "//*[@id='login']"
     sign_in_button_xpath = "//body[1]/div[1]/form[1]/div[1]/div[2]/button[1]/span[1]"
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
+    def type_in_password(self, password):
+        self.field_send_keys(self.password_xpath, password)
+    def click_on_the_sign_in_button(self):
+        self.click_on_the_element(self.sign_in_button_xpath)
+    def title_of_page(self):
+        assert self.get.page.title(self.login_url) == self.expected_title
+
+
---
+++ test_cases/login_to_the_system.py
@@ -0,0 +1,30 @@
+import os
+import unittest
+from selenium import webdriver
+
+from pages.login_page import LoginPage
+from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
+
+
+class TestLoginPage(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        os.chmod(DRIVER_PATH, 755)
+        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
+        self.driver.fullscreen_window()
+        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+
+    def test_log_in_to_the_system(self):
+        user_login = LoginPage(self.driver)
+        user_login.tytle_of_page()
+        user_login.tytle_of_page()
+        user_login.type_in_email('user10@getnada.com')
+        user_login.type_in_password('Test-1234')
+        user_login.click_on_the_sign_in_button()
+        time.sleep(5)
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
